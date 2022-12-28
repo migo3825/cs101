6169ff99729662de6329cabbd3715acd95c31d2e
migo3825 <120005531+migo3825@users.noreply.github.com>
2022-12-28 12:05:33 +0800

    Create main1.c

---
+++ HW6/main1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int i = 10;
+   if(i % 3==0)
+    printf("Love ");
+   if(i % 5==0)
+    printf("IU");
+    if(i % 3!=0 & i % 5!=0)
+    printf("%d\n", i); 
+ return 0;
+}
